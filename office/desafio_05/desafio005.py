f3f0e50c2b1a2608436af3b04942ea3b9f816e51
souzamateusdavi <nasc.souzamateusdavi@gmail.com>
2025-05-02 18:10:17 -0300

    add:calculadora

--- office/desafio_05/desafio005.py
+++ office/desafio_05/desafio005.py
@@ -1,1 +1,19 @@
-Teste desafio 005
+'''import matplotlib.pyplot as plt
+
+plt.figure(figsize=(12, 5))
+
+nomes = ["Mateus", "Lucas", "Gabriel", "Marcos"]
+rendimento = [10, 4, 8, 4]
+
+plt.bar(nomes, rendimento, color=blue)
+plt.title("Rendimento escolar")
+plt.ylabel("Rendimento")
+plt.xlabel("Nomes")
+
+plt.show'''
+print("Calculadora apenas com Soma")
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número:"))
+result = num1 + num2
+
+print(result)
