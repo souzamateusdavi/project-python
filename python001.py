0803fbe9e5514ba9b7ca63e57dd9ee9d768acfe1
Mateus Davi Nascimento de Souza <souzamateusdavi@outlook.com>
2025-05-05 18:25:46 -0300

    Desafio6

---
+++ python001.py
@@ -0,0 +1,19 @@
+num1 = int(input("Digite um número"))
+num2 = int(input("Digite o segundo número"))
+
+resultado_soma = num1 + num2
+resultado_subtração = num1 - num2
+resultado_multiplicação = num1 * num2
+resultado_divisão = num1 / num2
+
+print("O resultado da soma dos dois números é:")
+print(resultado_soma)
+pergunta1 = input("Veja o resultado da subtração: ")
+print("O resultado da subtração dos dois números é:")
+print(resultado_subtração)
+pergunta2 = input("Veja o resultado da multiplicaçao: ")
+print("O resultado da multiplicação é:")
+print(resultado_multiplicação)
+pergunta3 = input("Veja o resultado da divisão: ")
+print("O resultado da divisão é:")
+print(resultado_divisão)
